0692beeb8aa064fdcb119c370c560bf572108261
Antoine Aflalo <Belphemur@users.noreply.github.com>
2020-03-02 22:15:07 -0500

    Also merge timestamp

--- LogIngester/DnsIngest/Models/DnsLog.cs
+++ LogIngester/DnsIngest/Models/DnsLog.cs
@@ -18,19 +18,23 @@ namespace LogIngester.DnsIngest.Models
         [Timestamp]
         public DateTime? Timestamp { get; set; }
 
-        [Tag( "domain")]
+        [Tag("domain")]
         public string Domain { get; set; }
 
-        [Tag( "state")]
+        [Tag("state")]
         public QueryState State { get; set; }
 
-        [Field( "query")]
+        [Field("query")]
         public long Query { get; set; }
 
         public static DnsLog operator +(DnsLog a) => a;
 
         public static DnsLog operator +(DnsLog a, DnsLog b)
         {
+            if (a.Timestamp.HasValue && b.Timestamp.HasValue)
+                a.Timestamp = a.Timestamp.Value < b.Timestamp.Value ? a.Timestamp.Value : b.Timestamp.Value;
+            else if (!a.Timestamp.HasValue && b.Timestamp.HasValue)
+                a.Timestamp = b.Timestamp;
             a.Query += b.Query;
             return a;
         }
